a1d29a2b43759a1bb57bdb6d399ab2ac040f406f
Lenin Meza <merolhack@gmail.com>
2018-07-22 12:41:47 -0500

    Updates

--- src/services/auth0-variables.js
+++
@@ -1,6 +0,0 @@
-const AUTH_CONFIG = {
-  domain: 'merolhack.auth0.com',
-  clientId: 'K0UL8jvCr74MXpSy2VaWItpjYobAEjEq',
-  callbackUrl: 'http://localhost:3000/callback',
-};
-export default AUTH_CONFIG;

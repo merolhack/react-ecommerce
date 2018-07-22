12c5c36d4ba4ffda866a179462d6d4084a6944e7
Lenin Meza <merolhack@gmail.com>
2018-07-22 12:53:09 -0500

    :sparkles: core-js package added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+// Polyfills
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'raf/polyfill';
+// React + Redux
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
